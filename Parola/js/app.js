ae379ab8a300bf2746a917fb357dcfd274c25d71
longo2002 <lombardo.fra2002@gmail.com>
2023-02-09 08:54:26 +0100

    palindromia done

--- Parola/js/app.js
+++ Parola/js/app.js
@@ -3,30 +3,6 @@ let checkBtn = document.getElementById("checkBtn")
 const userWord = prompt("Inserisci una parola!")
 
 checkBtn.addEventListener("click",
-    // function checkPalindrome(str) {
-    //     // get the total length of the words  
-    //     const len = str.length;
-
-    //     for (let i = 0; i < len / 2; i++) {
-    //         if (str[i] !== str[len - 1 - i]) {
-    //             alert('It is not palindrome');
-    //             return;
-    //         }
-    //     }
-
-    //     alert('It is a palindrome');
-
-
-    //     const string = prompt('Enter a string or number to check for Palindrome: ');
-
-    //     const value = checkPalindrome(string);
-
-
-    //     console.log(value);
-    // }
-
-
-
     function wordCheck() {
         let length = userWord.length;
         for (let i = 0; i < length / 2; i++) {
